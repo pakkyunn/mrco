c012c1f8d340c1af54059d1fda5894500754400a
KIMWONBIN(김원빈) <diffngood@gmail.com>
2024-03-28 14:02:28 +0900

    commit

---
+++ app/src/main/java/kr/co/lion/team4/mrco/Tools.kt
@@ -0,0 +1,15 @@
+package kr.co.lion.team4.mrco
+
+class Tools {
+
+    companion object {
+
+    }
+
+}
+
+// MainActivity에서 보여줄 프레그먼트들의 이름
+enum class MainFragmentName(var str: String) {
+    COORDINATOR_INFO("Coordinator_info_Fragment"),
+    COORDINATOR_RANK("Coordinator_rank_Fragment"),
+}
