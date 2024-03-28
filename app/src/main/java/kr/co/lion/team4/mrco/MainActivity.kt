ed1726d8ae2c0d96ea9628901396a72c4eabb3aa
KIMWONBIN(김원빈) <diffngood@gmail.com>
2024-03-28 13:35:10 +0900

    init_main_commit
    초기세팅

---
+++ app/src/main/java/kr/co/lion/team4/mrco/MainActivity.kt
@@ -0,0 +1,17 @@
+package kr.co.lion.team4.mrco
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kr.co.lion.team4.mrco.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var activityMainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+    }
+}
