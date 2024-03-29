ee0a2ce56bd3d9c73b972d0511a2f085f18e82a5
Boram <dev.lboram@gmail.com>
2024-03-29 16:46:32 +0900

    [UI] 구매자-고객센터 - 1:1 문의 작성(CustomerInquiryFragment) 화면 구성

---
+++ app/src/main/java/kr/co/lion/team4/mrco/CustomerInquiryFragment.kt
@@ -0,0 +1,18 @@
+package kr.co.lion.team4.mrco
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/* (구매자) 고객센터 - 1:1 문의 작성 화면 */
+class CustomerInquiryFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_customer_inquiry, container, false)
+    }
+}
