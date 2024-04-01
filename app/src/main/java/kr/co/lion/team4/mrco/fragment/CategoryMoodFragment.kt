081f2012d9f0c1bf437a20a0f8b98f0e7f8a8dfb
oscar <oscarrr0225@gmail.com>
2024-04-01 16:06:36 +0900

    [Style] 각 xml에 해당하는 fragment 생성 및 fragment 패키지폴더 생성

---
+++ app/src/main/java/kr/co/lion/team4/mrco/fragment/CategoryMoodFragment.kt
@@ -0,0 +1,18 @@
+package kr.co.lion.team4.mrco.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kr.co.lion.team4.mrco.R
+
+class CategoryMoodFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_category_mood, container, false)
+    }
+}
