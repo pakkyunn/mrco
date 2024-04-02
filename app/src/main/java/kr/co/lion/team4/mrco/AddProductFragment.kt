7a23c6a2d9364fef04bd40daedc00c22b34390d4
Boram <dev.lboram@gmail.com>
2024-04-02 17:58:35 +0900

    [UI] 판매자 - 코디 상품 등록 (AddProductFragment) 화면 구성

---
+++ app/src/main/java/kr/co/lion/team4/mrco/AddProductFragment.kt
@@ -0,0 +1,18 @@
+package kr.co.lion.team4.mrco
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+/* (판매자) 코디 상품 등록 화면 */
+class AddProductFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_add_product, container, false)
+    }
+}
