75e757a897838367f2975fa2a51a23f9d8f34338
Boram <dev.lboram@gmail.com>
2024-04-03 17:04:56 +0900

    [Feat] (판매자) 상품 문의 답변 등록 화면 - mvvm 세팅

--- app/src/main/java/kr/co/lion/team4/mrco/RegisterQnaAnswerFragment.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/RegisterQnaAnswerFragment.kt
@@ -5,14 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import kr.co.lion.team4.mrco.databinding.FragmentRegisterQnaAnswerBinding
 
 /* (판매자) 상품 문의 답변 등록 화면 */
 class RegisterQnaAnswerFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register_qna_answer, container, false)
+    lateinit var fragmentRegisterQnaAnswerBinding: FragmentRegisterQnaAnswerBinding
+    lateinit var registerQnaAnswerViewModel: RegisterQnaAnswerViewModel
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        fragmentRegisterQnaAnswerBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_register_qna_answer, container, false)
+        registerQnaAnswerViewModel = RegisterQnaAnswerViewModel()
+        fragmentRegisterQnaAnswerBinding.registerQnaAnswerViewModel = registerQnaAnswerViewModel
+        fragmentRegisterQnaAnswerBinding.lifecycleOwner = this
+
+        return fragmentRegisterQnaAnswerBinding.root
     }
 }
