6ecc5ce466f7fdf095a3994131d542b4fe06c16d
Boram <dev.lboram@gmail.com>
2024-04-03 17:33:13 +0900

    [Feat] (구매자) 고객센터 1:1 문의 작성 화면 - mvvm 세팅 - View Model 클래스 생성 - xml에 data binding 적용

--- app/src/main/java/kr/co/lion/team4/mrco/CustomerInquiryFragment.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/CustomerInquiryFragment.kt
@@ -5,14 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import kr.co.lion.team4.mrco.databinding.FragmentCustomerInquiryBinding
 
 /* (구매자) 고객센터 - 1:1 문의 작성 화면 */
 class CustomerInquiryFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_customer_inquiry, container, false)
+    lateinit var fragmentCustomerInquiryBinding: FragmentCustomerInquiryBinding
+    lateinit var customerInquiryViewModel: CustomerInquiryViewModel
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        fragmentCustomerInquiryBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_customer_inquiry, container, false)
+        customerInquiryViewModel = CustomerInquiryViewModel()
+        fragmentCustomerInquiryBinding.customerInquiryViewModel = customerInquiryViewModel
+        fragmentCustomerInquiryBinding.lifecycleOwner = this
+
+        return fragmentCustomerInquiryBinding.root
     }
 }
