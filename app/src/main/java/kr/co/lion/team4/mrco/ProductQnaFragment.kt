29ed78db76569620ec62f6df99e555972bddd5bb
Boram <dev.lboram@gmail.com>
2024-04-03 16:22:00 +0900

    [Feat] (구매자) 상품 문의 화면 mvvm 세팅

--- app/src/main/java/kr/co/lion/team4/mrco/ProductQnaFragment.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/ProductQnaFragment.kt
@@ -5,14 +5,31 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import kr.co.lion.team4.mrco.databinding.FragmentProductQnaBinding
 
 class ProductQnaFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_qna, container, false)
+    lateinit var fragmetProductQnaBinding : FragmentProductQnaBinding
+    lateinit var productQnaViewModel: ProductQnaViewModel
+    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View? {
+
+        fragmetProductQnaBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_product_qna, container, false)
+        productQnaViewModel = ProductQnaViewModel()
+        fragmetProductQnaBinding.productQnaViewModel = productQnaViewModel
+        fragmetProductQnaBinding.lifecycleOwner = this
+
+        settingButtonPproductqnaSubmit()
+
+        return fragmetProductQnaBinding.root
     }
 
+    fun settingButtonPproductqnaSubmit(){
+        // 문의 내용 등록
+        fragmetProductQnaBinding.buttonProductqnaSubmit.setOnClickListener {
+            // 비밀글로 문의하는 지 여부
+            val isSecret = productQnaViewModel.checkbox_productqna_secret.value
+            // 문의 내용
+            val content = productQnaViewModel.edittext_productqna_content.value
+        }
+    }
 }
