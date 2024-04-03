003bd1edc6c6a6246a91358ac9b82f728094c57f
Boram <dev.lboram@gmail.com>
2024-04-03 14:23:23 +0900

    [feat] 상품 문의 내역 RecyclerView Adapter 추가

--- app/src/main/java/kr/co/lion/team4/mrco/ProductQnaListFragment.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/ProductQnaListFragment.kt
@@ -5,15 +5,55 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import kr.co.lion.team4.mrco.databinding.FragmentProductQnaListBinding
+import kr.co.lion.team4.mrco.databinding.ItemQnalistBinding
 
 
 /* (판매자) 상품 문의 내역 화면 */
 class ProductQnaListFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_product_qna_list, container, false)
+    lateinit var fragmentProductQnaListBinding: FragmentProductQnaListBinding
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        fragmentProductQnaListBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_product_qna_list, container, false)
+        // to do - viewModel setting
+        fragmentProductQnaListBinding.lifecycleOwner = this
+
+        return fragmentProductQnaListBinding.root
+    }
+
+    fun settingQnaListAdapter(){
+        fragmentProductQnaListBinding.recyclerviewQnaList.apply {
+            adapter = ProductQnaListRecyclerViewAdapter()
+            // to do - layoutManager = LinearLayoutManager("context")
+
+        }
+    }
+
+    inner class ProductQnaListRecyclerViewAdapter : RecyclerView.Adapter<ProductQnaListRecyclerViewAdapter.QnaListViewHolder>(){
+        inner class QnaListViewHolder(itemQnalistBinding:ItemQnalistBinding):RecyclerView.ViewHolder(itemQnalistBinding.root){
+            val itemQnalistBinding : ItemQnalistBinding
+            init {
+                this.itemQnalistBinding = itemQnalistBinding
+                this.itemQnalistBinding.root.layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QnaListViewHolder {
+            val itemQnalistBinding = ItemQnalistBinding.inflate(layoutInflater)
+            val qnaListViewHolder = QnaListViewHolder(itemQnalistBinding)
+
+            return qnaListViewHolder
+        }
+
+        override fun onBindViewHolder(holder: QnaListViewHolder, position: Int) {
+
+        }
+
+        override fun getItemCount(): Int {
+            return 10
+        }
     }
 }
