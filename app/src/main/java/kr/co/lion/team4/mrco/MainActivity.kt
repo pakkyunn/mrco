a9c7fe0f0815b0f7eb1f5534952dd07f6b617bca
KIMWONBIN(김원빈) <diffngood@gmail.com>
2024-04-04 22:49:31 +0900

    Fragment 추가

--- app/src/main/java/kr/co/lion/team4/mrco/MainActivity.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.google.android.material.transition.MaterialSharedAxis
 import kr.co.lion.team4.mrco.databinding.ActivityMainBinding
+import kr.co.lion.team4.mrco.fragment.MainHomeFragment
 
 
 class MainActivity : AppCompatActivity() {
@@ -23,25 +24,9 @@ class MainActivity : AppCompatActivity() {
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
 
-        // 인기 코디네이터, 코디네이터 소개, 코디네이터 메인 -완료
-        // 탭으로 화면 이동 가능 / 코디네이터 메인은 코디네이터 소개에서 하나 클릭하면 이동가능
+        // 메인 홈 화면
         replaceFragment(MainFragmentName.MAIN_HOME, false, false, null)
-        // replaceFragment(MainFragmentName.COORDINATOR_INFO, false, false, null)
-        // replaceFragment(MainFragmentName.COORDINATOR_MAIN, true, true, null)
 
-        // 좋아요 화면(코디네이터) - 완료
-        // replaceFragment(MainFragmentName.LIKE_COORDINATOR, false, false, null)
-
-        // 리뷰 작성 페이지 - 완료
-        // replaceFragment(MainFragmentName.WRITE_REVIEW, true, true, null)
-
-        // 주문 상세 정보 페이지 - 완료
-        // replaceFragment(MainFragmentName.ORDER_DETAIL, true, true, null)
-
-        // 매출관리(내역), 매출관리(캘린더) - 완료
-        // 탭으로 화면 이동 가능 / 리포트는 현재 연동 X
-        // replaceFragment(MainFragmentName.SALES_MANAGEMENT, false, false, null)
-        // replaceFragment(MainFragmentName.SALES_MANAGEMENT_CALENDAR, false, false, null)
     }
 
     // 지정한 Fragment를 보여주는 메서드
--- app/src/main/java/kr/co/lion/team4/mrco/fragment/HomeMbtiFragment.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/fragment/HomeMbtiFragment.kt
@@ -1,15 +1,17 @@
-package kr.co.lion.team4.mrco
+package kr.co.lion.team4.mrco.fragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kr.co.lion.team4.mrco.R
 
-class MainHomeFragment : Fragment() {
+class HomeMbtiFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_home, container, false)
+
+        return inflater.inflate(R.layout.fragment_home_mbti, container, false)
     }
 }
