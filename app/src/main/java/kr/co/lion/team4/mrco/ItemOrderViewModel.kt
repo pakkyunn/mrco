45e9e5536d35402f9da16d70c38a7b690dbbc583
Boram <dev.lboram@gmail.com>
2024-04-04 15:47:32 +0900

    [Feat] (구매자) 상품 주문 화면 - 주문상품목록 리사이클러뷰 mvvm 세팅 - RecyclerView Adapter 세팅 - View Model 클래스 생성 - xml에 data binding 적용

---
+++ app/src/main/java/kr/co/lion/team4/mrco/ItemOrderViewModel.kt
@@ -0,0 +1,15 @@
+package kr.co.lion.team4.mrco
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class ItemOrderViewModel : ViewModel() {
+    // 코디네이터명
+    val textviewOrderItemCoordinator = MutableLiveData<String>()
+    // 코디 상품명
+    val textviewOrderItemCoordi_name = MutableLiveData<String>()
+    // 옵션
+    val textviewOrderItemOption = MutableLiveData<String>()
+    // 가격
+    val textviewOrderItemPrice = MutableLiveData<String>()
+}
