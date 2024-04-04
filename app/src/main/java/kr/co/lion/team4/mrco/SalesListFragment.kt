cf4357ff86dbd670c66eff18fe16795205a677cb
Boram <dev.lboram@gmail.com>
2024-04-04 16:29:46 +0900

    [Feat] (판매자) 판매내역 화면 mvvm 세팅 - View Model 클래스 생성 - xml에 data binding 적용

--- app/src/main/java/kr/co/lion/team4/mrco/SalesListFragment.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/SalesListFragment.kt
@@ -5,14 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import kr.co.lion.team4.mrco.databinding.FragmentSalesListBinding
 
 /* (판매자) 판매내역 화면 */
 class SalesListFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sales_list, container, false)
+    lateinit var fragmentSalesListBinding: FragmentSalesListBinding
+    lateinit var salesListViewModel: SalesListViewModel
+
+    lateinit var mainActivity: MainActivity
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        fragmentSalesListBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_sales_list, container, false)
+        salesListViewModel = SalesListViewModel()
+        fragmentSalesListBinding.salesListViewModel = salesListViewModel
+        fragmentSalesListBinding.lifecycleOwner = this
+
+        mainActivity = activity as MainActivity
+
+        return fragmentSalesListBinding.root
     }
 }
