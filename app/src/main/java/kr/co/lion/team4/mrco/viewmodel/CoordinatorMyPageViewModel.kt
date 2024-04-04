517993997122d78bd7c88f3220683fb64ec6724c
oscar <oscarrr0225@gmail.com>
2024-04-04 14:55:08 +0900

    [Feat] -각 화면 MVVM 세팅 -ViewModel 생성 -중복된 row xml파일 제거

---
+++ app/src/main/java/kr/co/lion/team4/mrco/viewmodel/CoordinatorMyPageViewModel.kt
@@ -0,0 +1,15 @@
+package kr.co.lion.team4.mrco.viewmodel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class CoordinatorMyPageViewModel: ViewModel() {
+    // 코디네이터 이름
+    val textViewNameCoordinatorMyPage = MutableLiveData<String>()
+    // 성별
+    val textViewGenderCoordinatorMyPage = MutableLiveData<String>()
+    // MBTI
+    val textViewMbtiCoordinatorMyPage = MutableLiveData<String>()
+    // 소개글
+    val textViewIntroductionCoordinatorMyPage = MutableLiveData<String>()
+}
