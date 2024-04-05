62591c1fbd239c05241e2aca798fcf418d30cdad
Boram <dev.lboram@gmail.com>
2024-04-05 15:49:46 +0900

    [UI] (구매자) 주문내역 화면 mvvm 세팅 - View Model 클래스 생성 - xml에 data binding 적용

--- app/src/main/java/kr/co/lion/team4/mrco/OrderHistoryFragment.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/OrderHistoryFragment.kt
@@ -5,16 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import kr.co.lion.team4.mrco.databinding.FragmentOrderHistoryBinding
 
 /* (구매자) 주문 내역 화면 */
 class OrderHistoryFragment : Fragment() {
+    lateinit var fragmentOrderHistoryBinding: FragmentOrderHistoryBinding
+    lateinit var orderHistoryViewModel : OrderHistoryViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order_history, container, false)
+    lateinit var mainActivity: MainActivity
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        fragmentOrderHistoryBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_order_history, container, false)
+        orderHistoryViewModel = OrderHistoryViewModel()
+        fragmentOrderHistoryBinding.orderHistoryViewModel = orderHistoryViewModel
+        fragmentOrderHistoryBinding.lifecycleOwner = this
+
+        mainActivity = activity as MainActivity
+
+        return fragmentOrderHistoryBinding.root
     }
 
 
