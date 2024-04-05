29daaf67e420fc19a2a9e6fabd38ef7eacfa6a02
KIMWONBIN(김원빈) <diffngood@gmail.com>
2024-04-05 17:32:08 +0900

    commit

--- app/src/main/java/kr/co/lion/team4/mrco/fragment/home/recommend/HomeRecommendFragment.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/fragment/home/recommend/HomeRecommendFragment.kt
@@ -1,10 +1,15 @@
 package kr.co.lion.team4.mrco.fragment.home.recommend
 
+import android.graphics.Rect
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.tabs.TabLayout
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -14,6 +19,9 @@ import kr.co.lion.team4.mrco.MainFragmentName
 import kr.co.lion.team4.mrco.R
 import kr.co.lion.team4.mrco.databinding.FragmentHomeMbtiBinding
 import kr.co.lion.team4.mrco.databinding.FragmentHomeRecommendBinding
+import kr.co.lion.team4.mrco.databinding.RowCoordinatorInfoBinding
+import kr.co.lion.team4.mrco.databinding.RowHomeRecommendBinding
+import kr.co.lion.team4.mrco.viewmodel.coordinator.RowCoordinatorInfoViewModel
 
 class HomeRecommendFragment : Fragment() {
 
@@ -26,9 +34,30 @@ class HomeRecommendFragment : Fragment() {
         fragmentHomeRecommendBinding = FragmentHomeRecommendBinding.inflate(inflater)
         mainActivity = activity as MainActivity
 
+        // 툴바, 하단바, 탭 관련
         settingMainTab()
 
+        // 리사이클러 뷰
+        settingRecyclerViewHomeRecommend()
+
         return fragmentHomeRecommendBinding.root
+    }
+
+    // 홈 추천 화면 리사이클러 뷰 설정
+    fun settingRecyclerViewHomeRecommend() {
+        fragmentHomeRecommendBinding.apply {
+            homeRecommendRecycler.apply {
+                // 어뎁터 및 레이아웃 매니저 설정
+                adapter = HomeRecommendRecyclerViewAdapter()
+            }
+        }
+    }
+
+    class HorizontalSpacingItemDecoration(private val spacing: Int) : RecyclerView.ItemDecoration() {
+        override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+            super.getItemOffsets(outRect, view, parent, state)
+            outRect.right = spacing
+        }
     }
 
     fun settingMainTab(){
@@ -68,6 +97,40 @@ class HomeRecommendFragment : Fragment() {
                     }
                 })
             }
+        }
+    }
+
+
+    // 코디네이터 소개 리사이클러 뷰 어뎁터 home_recommend_recycler
+    inner class HomeRecommendRecyclerViewAdapter: RecyclerView.Adapter<HomeRecommendRecyclerViewAdapter.HomeRecommendViewHolder>(){
+        inner class HomeRecommendViewHolder(rowHomeRecommendBinding: RowHomeRecommendBinding): RecyclerView.ViewHolder(rowHomeRecommendBinding.root){
+            val rowHomeRecommendBinding: RowHomeRecommendBinding
+
+            init {
+                this.rowHomeRecommendBinding = rowHomeRecommendBinding
+
+                this.rowHomeRecommendBinding.root.layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeRecommendViewHolder {
+            // val rowCoordinatorInfoBinding = RowCoordinatorInfoBinding.inflate(layoutInflater)
+
+            val rowHomeRecommendBinding = RowHomeRecommendBinding.inflate(layoutInflater)
+            val homeRecommendViewHolder = HomeRecommendViewHolder(rowHomeRecommendBinding)
+
+            return homeRecommendViewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return 4
+        }
+
+        override fun onBindViewHolder(holder: HomeRecommendViewHolder, position: Int) {
+
         }
     }
 }
