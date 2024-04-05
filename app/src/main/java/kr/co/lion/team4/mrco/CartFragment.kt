9d773e45c54d0e038bce6f7d8187c24693533340
Boram <dev.lboram@gmail.com>
2024-04-05 18:04:16 +0900

    [Feat] (구매자) 장바구니 화면 mvvm 세팅 - View Model 클래스 생성 - xml에 data binding 적용

--- app/src/main/java/kr/co/lion/team4/mrco/CartFragment.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/CartFragment.kt
@@ -5,15 +5,25 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import kr.co.lion.team4.mrco.databinding.FragmentCartBinding
 
 /* (구매자) 장바구니 화면 */
 class CartFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart, container, false)
+    lateinit var fragmentCartBinding: FragmentCartBinding
+    lateinit var cartViewModel : CartViewModel
+
+    lateinit var mainActivity: MainActivity
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        fragmentCartBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_cart, container, false)
+        cartViewModel = CartViewModel()
+        fragmentCartBinding.cartViewModel = cartViewModel
+        fragmentCartBinding.lifecycleOwner = this
+
+        mainActivity = activity as MainActivity
+
+        return fragmentCartBinding.root
     }
 
 }
