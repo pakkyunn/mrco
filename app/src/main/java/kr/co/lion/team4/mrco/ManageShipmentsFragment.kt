74509c9355c1c259029095edde32b2af9526d63a
Boram <dev.lboram@gmail.com>
2024-04-05 14:27:47 +0900

    [UI] (판매자) 배송관리 화면 mvvm 세팅 - View Model 클래스 생성 - xml에 data binding 적용

--- app/src/main/java/kr/co/lion/team4/mrco/ManageShipmentsFragment.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/ManageShipmentsFragment.kt
@@ -5,14 +5,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import kr.co.lion.team4.mrco.databinding.FragmentManageShipmentsBinding
 
 /* (판매자) 배송관리 화면 - 운송장 번호를 등록할 수 있는 화면 */
 class ManageShipmentsFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_manage_shipments, container, false)
+    lateinit var fragmentManageShipmentsBinding : FragmentManageShipmentsBinding
+    lateinit var manageShipmentsViewModel: ManageShipmentsViewModel
+
+    lateinit var mainActivity: MainActivity
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        fragmentManageShipmentsBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_manage_shipments, container, false)
+        manageShipmentsViewModel = ManageShipmentsViewModel()
+        fragmentManageShipmentsBinding.manageShipmentsViewModel = manageShipmentsViewModel
+        fragmentManageShipmentsBinding.lifecycleOwner = this
+
+        // Activity
+        mainActivity = activity as MainActivity
+
+        return fragmentManageShipmentsBinding.root
     }
 }
