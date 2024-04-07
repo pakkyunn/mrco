885ffe7440034b4e14aa728a5b0aa68248b4f914
Boram <dev.lboram@gmail.com>
2024-04-07 22:21:53 +0900

    [Feat] (구매자) 코디 상품 등록 화면 AddProductFragment - mvvm 세팅 - View Model 클래스 생성 - xml에 data binding 적용

--- app/src/main/java/kr/co/lion/team4/mrco/AddProductFragment.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/AddProductFragment.kt
@@ -5,14 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import kr.co.lion.team4.mrco.databinding.FragmentAddProductBinding
 
 /* (판매자) 코디 상품 등록 화면 */
 class AddProductFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_product, container, false)
+    lateinit var fragmentAddProductBinding: FragmentAddProductBinding
+    lateinit var addProductViewModel: AddProductViewModel
+
+    lateinit var mainActivity: MainActivity
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        fragmentAddProductBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_add_product, container, false)
+        addProductViewModel = AddProductViewModel()
+        fragmentAddProductBinding.addProductViewModel = addProductViewModel
+        fragmentAddProductBinding.lifecycleOwner = this
+
+        mainActivity = activity as MainActivity
+
+        return fragmentAddProductBinding.root
     }
 }
