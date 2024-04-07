32b16237185e374ffc6e93ec3ae3f102a4b1b0d1
문태진 <mtj1015@naver.com>
2024-04-07 20:32:17 +0900

    [Fix] 리뷰 MVVM 적용

--- app/src/main/java/kr/co/lion/team4/mrco/viewmodel/ProductReviewViewModel.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/viewmodel/ProductReviewViewModel.kt
@@ -6,25 +6,4 @@ import androidx.lifecycle.ViewModel
 
 // Model : 프로젝트에서 사용하는 모든 데이터를 관리
 class ProductReviewViewModel: ViewModel(){
-    // UI 요소에 셋팅할 데이터들을 관리하는 데이터
-    // Activity or Fragment와 XML 파일의 오작교 역할
-
-    // View에 설정할 값을 담을 객체
-    // 제네릭은 반드시 View의 속성에 대한 값의 타입으로 맞춰줘야 한다
-    val editData1 = MutableLiveData<String>()
-    val editData2 = MutableLiveData<String>()
-    val textViewData = MutableLiveData<String>()
-
-    // 버튼을 누르면 동작하는 메서드
-    fun buttonClick(view: View){
-        // 입력한 값을 가져온다.
-        val data1 = editData1.value?.toInt()!!
-        val data2 = editData2.value?.toInt()!!
-
-        val r1 = data1 + data2
-
-        // 출력한다
-        textViewData.value = r1.toString()
-    }
-
 }
--- app/src/main/java/kr/co/lion/team4/mrco/viewmodel/ReviewViewModel.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/viewmodel/ReviewViewModel.kt
@@ -1,6 +1,34 @@
 package kr.co.lion.team4.mrco.viewmodel
 
+import android.graphics.drawable.Drawable
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class ReviewViewModel: ViewModel() {
+
+    var reviewProductImage = MutableLiveData<Drawable>()
+
+    var reviewDate = MutableLiveData<String>() // 작성일자
+
+    var reviewProductName = MutableLiveData<String>() // 코디 이름
+
+    var reviewProductMBTI = MutableLiveData<String>() // 스타일링 / MBTI
+
+    var reviewStar = MutableLiveData<Int>() // 별 갯수
+
+    var reviewProductSize = MutableLiveData<String>()
+
+    var reviewProductBright = MutableLiveData<String>()
+
+    var reviewProductColor = MutableLiveData<String>()
+
+    var reviewProductPack = MutableLiveData<String>()
+
+    var reviewProductThickness = MutableLiveData<String>()
+
+    var reviewProductDelivery = MutableLiveData<String>()
+
+    var reviewContent = MutableLiveData<String>()
+
+
 }
