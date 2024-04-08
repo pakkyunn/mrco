4719c6bcd4abd9c76fbf7d056d7928e344c12c0c
KIMWONBIN(김원빈) <diffngood@gmail.com>
2024-04-08 17:08:14 +0900

    commit

--- app/src/main/java/kr/co/lion/team4/mrco/fragment/mypage/UserMyPageFragment.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/fragment/mypage/UserMyPageFragment.kt
@@ -31,6 +31,7 @@ class UserMyPageFragment : Fragment() {
         mainActivity = activity as MainActivity
 
         settingUserMyPageMenuClickEvent()
+        mainActivity.viewBottomSheet()
 
         return fragmentUserMyPageBinding.root
     }
