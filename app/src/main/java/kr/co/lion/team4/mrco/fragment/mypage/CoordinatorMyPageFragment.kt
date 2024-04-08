6c20ed18a0391c3d4f814d82f50cffd1b5a2cd27
Boram <dev.lboram@gmail.com>
2024-04-08 15:38:44 +0900

    [Feat] 코디네이터 마이페이지 - 각 메뉴에 화면 이동하는 클릭 이벤트 추가 - 판매자 마이페이지 -> 1:1 문의 내역, 배송 관리, 판매 내역, 코디 상품 등록

--- app/src/main/java/kr/co/lion/team4/mrco/fragment/mypage/CoordinatorMyPageFragment.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/fragment/mypage/CoordinatorMyPageFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import kr.co.lion.team4.mrco.MainActivity
+import kr.co.lion.team4.mrco.MainFragmentName
 import kr.co.lion.team4.mrco.R
 import kr.co.lion.team4.mrco.databinding.FragmentCoordinatorMyPageBinding
 import kr.co.lion.team4.mrco.viewmodel.mypage.CoordinatorMyPageViewModel
@@ -29,6 +30,35 @@ class CoordinatorMyPageFragment : Fragment() {
 
         mainActivity = activity as MainActivity
 
+        settingCoordiMyPageMenuClickEvent()
+
         return fragmentCoordinatorMyPageBinding.root
+    }
+
+    fun settingCoordiMyPageMenuClickEvent(){
+        fragmentCoordinatorMyPageBinding.apply {
+            // 코디 상품 등록
+            textViewMenuCoordinatorMyPage1.setOnClickListener {
+                mainActivity.replaceFragment(MainFragmentName.ADD_PRODUCT_FRAGMENT, true, true, null)
+            }
+
+            // 등록 상품 관리
+
+            // 배송 관리
+            textViewMenuCoordinatorMyPage3.setOnClickListener {
+                mainActivity.replaceFragment(MainFragmentName.MANAGE_SHIPMENT_FRAGMENT, true, true, null)
+            }
+
+            // 상품 문의 관리
+            textViewMenuCoordinatorMyPage4.setOnClickListener {
+                mainActivity.replaceFragment(MainFragmentName.PRODUCT_QNA_LIST_FRAGMENT, true, true, null)
+            }
+
+            // 상품 판매 내역
+            textViewMenuCoordinatorMyPage5.setOnClickListener {
+                mainActivity.replaceFragment(MainFragmentName.SALES_LIST_FRAGMENT, true, true, null)
+            }
+            // 매출/정산 관리
+        }
     }
 }
