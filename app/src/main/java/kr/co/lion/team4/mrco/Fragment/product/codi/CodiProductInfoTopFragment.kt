fcd6c582f6a8e467fb636247b9d1d7a693126fbc
문태진 <mtj1015@naver.com>
2024-04-08 17:31:17 +0900

    [Fix] Fragment 수정

--- app/src/main/java/kr/co/lion/team4/mrco/Fragment/product/codi/CodiProductInfoTopFragment.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/Fragment/product/codi/CodiProductInfoTopFragment.kt
@@ -8,9 +8,12 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.get
+import androidx.recyclerview.widget.RecyclerView
 import kr.co.lion.team4.mrco.Activity.MainActivity
+import kr.co.lion.team4.mrco.ProductCategoryLinkedList
 import kr.co.lion.team4.mrco.R
 import kr.co.lion.team4.mrco.databinding.FragmentCodiProductInfoTopBinding
+import kr.co.lion.team4.mrco.databinding.RowCodiProductInfoBinding
 import kr.co.lion.team4.mrco.viewmodel.CodiProductInfoTopViewModel
 
 class CodiProductInfoTopFragment : Fragment() {
@@ -18,6 +21,9 @@ class CodiProductInfoTopFragment : Fragment() {
     private lateinit var binding: FragmentCodiProductInfoTopBinding
     private lateinit var viewModel: CodiProductInfoTopViewModel
     private lateinit var mainActivity: MainActivity
+    // 상품의 정보를 담고있는 리스트
+    var productTopList = mutableListOf<ProductCategoryLinkedList>()
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
@@ -28,5 +34,34 @@ class CodiProductInfoTopFragment : Fragment() {
 
 
         return binding.root
+    }
+
+
+    inner class CodiProductInfoTopAdapater: RecyclerView.Adapter<CodiProductInfoTopAdapater.TopViewHolder>(){
+        inner class TopViewHolder(rowCodiProductInfoBinding: RowCodiProductInfoBinding): RecyclerView.ViewHolder(rowCodiProductInfoBinding.root){
+            val rowCodiProductInfoBinding: RowCodiProductInfoBinding
+            init {
+                this.rowCodiProductInfoBinding = rowCodiProductInfoBinding
+
+                this.rowCodiProductInfoBinding.root.layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+            }
+        }
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CodiProductInfoTopAdapater.TopViewHolder {
+            val rowCodiProductInfoBinding = RowCodiProductInfoBinding.inflate(layoutInflater)
+            val topViewHolder = TopViewHolder(rowCodiProductInfoBinding)
+            return topViewHolder
+        }
+
+        override fun onBindViewHolder(holder: CodiProductInfoTopAdapater.TopViewHolder, position: Int) {
+            TODO("Not yet implemented")
+        }
+
+        override fun getItemCount(): Int {
+            TODO("Not yet implemented")
+        }
+
     }
 }
--- app/src/main/java/kr/co/lion/team4/mrco/Tools.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/Tools.kt
@@ -15,3 +15,20 @@ enum class FragmentName(var str: String){
     FRAGMENT_CODI_PRODUCT_INFO_ACCESSORY("CodiProductInfoAccessoryFragment"),
 
 }
+
+enum class ProductSize(var ps: String){
+    SIZE_XXXSMALL("XXXS"),
+    SIZE_XXSMALL("XXS"),
+    SIZE_XSMALL("XS"),
+    SIZE_SMALL("S"),
+    SIZE_MEDIUM("M"),
+    SIZE_LARGE("L"),
+    SIZE_XLARGE("XL"),
+    SIZE_XXLARGE("XXL"),
+    SIZE_XXXLARGE("XXXL")
+}
+
+enum class ProductColor(var pc: String){
+    COLOR_BLACK("Black"),
+    COLOR_WHITE("White")
+}
