6a2d46ee75d8fa8cee1620ac72255ae45f863315
Boram <dev.lboram@gmail.com>
2024-04-08 10:51:52 +0900

    [Feat] 프래그먼트 목록에 추가된 프래그먼트 이름 추가

--- app/src/main/java/kr/co/lion/team4/mrco/Tools.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/Tools.kt
@@ -29,7 +29,9 @@ enum class MainFragmentName(var str: String) {
 
     SALES_MANAGEMENT("Sales_Management_Fragment"),
     SALES_MANAGEMENT_CALENDAR("Sales_Management_Calendar_Fragment"),
-  
+    MANAGE_SHIPMENT_FRAGMENT("ManageShipmentsFragment"), // 판매자 - 배송관리 화면
+    SALES_LIST_FRAGMENT("SalesListFragment"), // 판매 내역 화면
+
     APP_NOTICE_FRAGMENT("AppNoticeFragment"),
 
     CATEGORY_FRAGMENT("CategoryFragment"),
@@ -38,4 +40,17 @@ enum class MainFragmentName(var str: String) {
     USER_MYPAGE_FRAGMENT("UserMyPageFragment"),
 
     CUSTOMER_SERVICE_FRAGMENT("CustomerServiceFragment"),
+    CUSTOMER_INQUIRY_FRAGMENT("CustomerInquiryFragment"), // 고객센터 1:1 문의 작성 화면
+
+    CART_FRAGMENT("CartFragment"), //장바구니
+
+    ORDER_FRAGMENT("OrderFragment"), // 주문,결제 화면
+    ORDER_HISTORY_FRAGMENT("OrderHistoryFragment"), // 구매자 주문내역 화면
+
+    // 상품 1:1 문의
+    REGISTER_PRODUCT_QNA_FRAGMENT("RegisterProductQnaFragment"), // 상품 1:1문의 작성화면
+    PRODUCT_QNA_LIST_FRAGMENT("ProductQnaListFragment"), // 판매자 - 상품 문의 내역 화면
+    REGISTER_QNA_ANSWER_FRAGMENT("RegisterQnaAnswerFragment"), // 상품 문의 답변 등록 화면
+
+    ADD_PRODUCT_FRAGMENT("AddProductFragment"), // 상품등록 화면
 }
