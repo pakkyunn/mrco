91ec26cb789bcb59ba7f922ba2e5c7f8ea653628
Boram <dev.lboram@gmail.com>
2024-04-09 15:35:31 +0900

    [UI] 화면 상단에 툴바 추가

--- app/src/main/java/kr/co/lion/team4/mrco/fragment/customerService/CustomerInquiryFragment.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/fragment/customerService/CustomerInquiryFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import kr.co.lion.team4.mrco.MainActivity
+import kr.co.lion.team4.mrco.MainFragmentName
 import kr.co.lion.team4.mrco.viewmodel.customerService.CustomerInquiryViewModel
 import kr.co.lion.team4.mrco.R
 import kr.co.lion.team4.mrco.databinding.FragmentCustomerInquiryBinding
@@ -14,6 +16,8 @@ import kr.co.lion.team4.mrco.databinding.FragmentCustomerInquiryBinding
 class CustomerInquiryFragment : Fragment() {
     lateinit var fragmentCustomerInquiryBinding: FragmentCustomerInquiryBinding
     lateinit var customerInquiryViewModel: CustomerInquiryViewModel
+
+    lateinit var mainActivity: MainActivity
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         fragmentCustomerInquiryBinding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_customer_inquiry, container, false)
@@ -21,6 +25,27 @@ class CustomerInquiryFragment : Fragment() {
         fragmentCustomerInquiryBinding.customerInquiryViewModel = customerInquiryViewModel
         fragmentCustomerInquiryBinding.lifecycleOwner = this
 
+        mainActivity = activity as MainActivity
+
+        // 툴바, bottom navigation 설정
+        settingCustomerInquiryToolbar()
+        mainActivity.removeBottomSheet()
+
         return fragmentCustomerInquiryBinding.root
+    }
+
+    // 툴바 설정
+    fun settingCustomerInquiryToolbar(){
+        fragmentCustomerInquiryBinding.toolbarCustomerInquiry.apply {
+            setNavigationIcon(R.drawable.arrow_back_24px)
+            setNavigationOnClickListener {
+                backProcess()
+            }
+        }
+    }
+
+    // 뒤로가기
+    fun backProcess(){
+        mainActivity.removeFragment(MainFragmentName.CUSTOMER_INQUIRY_FRAGMENT)
     }
 }
