ddabf8c1d0b5703ee529ea1477f03a9fd6ca96d1
Boram <dev.lboram@gmail.com>
2024-04-11 13:26:31 +0900

    [UI] 상품 문의 답변 등록화면에 toolbar 추가, 답변 등록 버튼 ui 변경

--- app/src/main/java/kr/co/lion/team4/mrco/fragment/productQna/RegisterQnaAnswerFragment.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/fragment/productQna/RegisterQnaAnswerFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import kr.co.lion.team4.mrco.MainActivity
+import kr.co.lion.team4.mrco.MainFragmentName
 import kr.co.lion.team4.mrco.R
 import kr.co.lion.team4.mrco.viewmodel.productQna.RegisterQnaAnswerViewModel
 import kr.co.lion.team4.mrco.databinding.FragmentRegisterQnaAnswerBinding
@@ -14,6 +16,9 @@ import kr.co.lion.team4.mrco.databinding.FragmentRegisterQnaAnswerBinding
 class RegisterQnaAnswerFragment : Fragment() {
     lateinit var fragmentRegisterQnaAnswerBinding: FragmentRegisterQnaAnswerBinding
     lateinit var registerQnaAnswerViewModel: RegisterQnaAnswerViewModel
+
+    lateinit var mainActivity: MainActivity
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         fragmentRegisterQnaAnswerBinding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_register_qna_answer, container, false)
@@ -21,6 +26,24 @@ class RegisterQnaAnswerFragment : Fragment() {
         fragmentRegisterQnaAnswerBinding.registerQnaAnswerViewModel = registerQnaAnswerViewModel
         fragmentRegisterQnaAnswerBinding.lifecycleOwner = this
 
+        mainActivity = activity as MainActivity
+
+        // 툴바, 하단바 설정
+        settingToolbarRegisterQnaAnswer()
+        mainActivity.removeBottomSheet()
         return fragmentRegisterQnaAnswerBinding.root
+    }
+
+    fun settingToolbarRegisterQnaAnswer(){
+        fragmentRegisterQnaAnswerBinding.toolbarRegisterQnaAnswer.apply {
+            setNavigationIcon(R.drawable.arrow_back_24px)
+            setNavigationOnClickListener {
+                backProcess()
+            }
+        }
+    }
+
+    fun backProcess(){
+        mainActivity.removeFragment(MainFragmentName.REGISTER_QNA_ANSWER_FRAGMENT)
     }
 }
