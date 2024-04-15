425588e2d16d095e4028c4ad43389e683c47f961
wtoshm <wtoshm@gmail.com>
2024-04-15 15:05:02 +0900

    LoginFragment databinding 및 로그인 처리

--- app/src/main/java/kr/co/lion/team4/mrco/fragment/LoginFragment.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/fragment/LoginFragment.kt
@@ -1,15 +1,26 @@
 package kr.co.lion.team4.mrco.fragment
 
-import android.graphics.Color
+import android.content.Context
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
-import kr.co.lion.team4.mrco.databinding.FragmentLoginBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import kr.co.lion.team4.mrco.MainActivity
 import kr.co.lion.team4.mrco.MainFragmentName
+import kr.co.lion.team4.mrco.R
+import kr.co.lion.team4.mrco.Tools
+import kr.co.lion.team4.mrco.UserState
+import kr.co.lion.team4.mrco.dao.UserDao
+import kr.co.lion.team4.mrco.databinding.FragmentLoginBinding
+import kr.co.lion.team4.mrco.viewmodel.LoginViewModel
 
 
 class LoginFragment : Fragment() {
@@ -17,15 +28,23 @@ class LoginFragment : Fragment() {
     lateinit var fragmentLoginBinding: FragmentLoginBinding
     lateinit var mainActivity: MainActivity
 
+    lateinit var loginViewModel: LoginViewModel
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
+        //fragmentLoginBinding = FragmentLoginBinding.inflate(inflater)
+        fragmentLoginBinding = DataBindingUtil.inflate(layoutInflater, R.layout.fragment_login, container, false)
+        loginViewModel = LoginViewModel()
+        fragmentLoginBinding.loginViewModel = loginViewModel
+        fragmentLoginBinding.lifecycleOwner = this
 
-        fragmentLoginBinding = FragmentLoginBinding.inflate(inflater)
+
         mainActivity = activity as MainActivity
 
         // 하단 바 제거
         mainActivity.removeBottomSheet()
 
+        settingLoginInput()
         settingButtonLoginJoin()
         settingButtonLoginSubmit()
 
@@ -54,9 +73,99 @@ class LoginFragment : Fragment() {
     }
 
     fun settingButtonLoginSubmit() {
-        //테스트 용 회원정보 수정 화면으로 이동
+//        //테스트 용 회원정보 수정 화면으로 이동
+//        fragmentLoginBinding.buttonLoginSubmit.setOnClickListener {
+//            mainActivity.replaceFragment(MainFragmentName.HOME_RECOMMEND, false, false, null )
+//        }
         fragmentLoginBinding.buttonLoginSubmit.setOnClickListener {
-            mainActivity.replaceFragment(MainFragmentName.HOME_RECOMMEND, false, false, null )
+            // 유효성 검사
+            val validation = checkLoginInput()
+
+            if (validation) {
+                loginProcess()
+            }
+        }
+    }
+
+    // 유효성 검사
+    fun checkLoginInput():Boolean{
+
+        // 입력한 값들을 가져온다.
+        val userId = loginViewModel.textFieldLoginUserId.value!!
+        val userPw = loginViewModel.textFieldLoginUserPw.value!!
+
+        if(userId.isEmpty()){
+            Tools.showErrorDialog(mainActivity, fragmentLoginBinding.textFieldLoginUserId, "아이디 입력 오류",
+                "아이디를 입력해주세요")
+            return false
+        }
+        if(userPw.isEmpty()){
+            Tools.showErrorDialog(mainActivity, fragmentLoginBinding.textFieldLoginUserPw, "비밀번호 입력 오류",
+                "비밀번호를 입력해주세요")
+            return false
+        }
+
+        return true
+    }
+
+    // 입력 요소들 초기화
+    fun settingLoginInput(){
+        loginViewModel.textFieldLoginUserId.value = ""
+        loginViewModel.textFieldLoginUserPw.value = ""
+    }
+
+    // 로그인 처리
+    fun loginProcess(){
+        // 사용자가 입력한 정보를 가져온다.
+        val userId = loginViewModel.textFieldLoginUserId.value!!
+        val userPw = loginViewModel.textFieldLoginUserPw.value!!
+        val job1 = CoroutineScope(Dispatchers.Main).launch {
+            val loginUserModel = UserDao.getUserDataById(userId)
+            // 만약 null 이라면..
+            if(loginUserModel == null){
+                Tools.showErrorDialog(mainActivity, fragmentLoginBinding.textFieldLoginUserId, "로그인 오류",
+                    "존재하지 않는 아이디 입니다")
+            }
+            // 만약 정보를 가져온 것이 있다면
+            else {
+                // 입력한 비밀번호와 서버에서 받아온 사용자의 비밀호가 다르다면..
+                if(userPw != loginUserModel.userPw){
+                    Tools.showErrorDialog(mainActivity, fragmentLoginBinding.textFieldLoginUserPw, "로그인 오류",
+                        "비밀번호가 잘못되었습니다")
+                }
+                // 비밀번호가 일치한다면
+                else {
+
+                    // 회원이 탈퇴 상태라면
+                    if(loginUserModel.userState == UserState.USER_STATE_SIGNOUT.num){
+                        MaterialAlertDialogBuilder(mainActivity).apply {
+                            setTitle("로그인 오류")
+                            setMessage("탈퇴한 회원입니다")
+                            setPositiveButton("확인"){ dialogInterface: DialogInterface, i: Int ->
+                                loginViewModel.textFieldLoginUserId.value = ""
+                                loginViewModel.textFieldLoginUserPw.value = ""
+                                Tools.showSoftInput(mainActivity, fragmentLoginBinding.textFieldLoginUserId)
+                            }
+                            show()
+                        }
+                    } else {
+                        // 자동 로그인이 체크되어 있다면...
+                        if (loginViewModel.checkBoxLoginAutoLogin.value == true) {
+                            // Preferences 에 사용자 정보를 저장해둔다.
+                            val sharedPreferences =
+                                mainActivity.getSharedPreferences("AutoLogin", Context.MODE_PRIVATE)
+                            val editor = sharedPreferences.edit()
+                            editor.putInt("loginUserIdx", loginUserModel.userIdx)
+                            editor.putString("loginUserName", loginUserModel.userName)
+                            editor.apply()
+                        }
+                    }
+
+                    Snackbar.make(fragmentLoginBinding.root, "로그인에 성공하였습니다", Snackbar.LENGTH_SHORT).show()
+                    // 홈화면 추천화면 fragment로 이동
+                    mainActivity.replaceFragment(MainFragmentName.HOME_RECOMMEND, true, true, null)
+                }
+            }
         }
     }
 
