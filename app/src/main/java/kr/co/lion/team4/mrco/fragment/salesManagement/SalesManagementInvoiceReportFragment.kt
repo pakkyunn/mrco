09b7cb8870aded1c046c3164e972a131c529a438
Boram <dev.lboram@gmail.com>
2024-04-15 16:08:50 +0900

    [Refactor] 레이아웃 변경에 따른 불필요한 메서드, import 제거 - view model -> 불필요한 항목 제거

--- app/src/main/java/kr/co/lion/team4/mrco/fragment/salesManagement/SalesManagementInvoiceReportFragment.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/fragment/salesManagement/SalesManagementInvoiceReportFragment.kt
@@ -6,7 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
-import com.google.android.material.tabs.TabLayout
 import kr.co.lion.team4.mrco.MainActivity
 import kr.co.lion.team4.mrco.MainFragmentName
 import kr.co.lion.team4.mrco.R
@@ -30,57 +29,10 @@ class SalesManagementInvoiceReportFragment : Fragment() {
 
         // 툴바, 하단바, 탭 관련
         mainActivity.removeBottomSheet()
-        toolbarSetting()
-        settingTabs()
-        settingSalesManagementTab()
 
         return fragmentInvoiceReportBinding.root
     }
 
-    fun toolbarSetting(){
-        fragmentInvoiceReportBinding.toolbarInvoiceReport.apply {
-            setNavigationOnClickListener {
-                backProcess()
-            }
-        }
-    }
-
-    // 탭바 위치 설정
-    fun settingTabs(){
-        fragmentInvoiceReportBinding.apply {
-            val tabLayout = tabsInvoiceReport
-            tabLayout.getTabAt(0)?.select()
-        }
-    }
-
-    // 상단 탭 선택 설정
-    fun settingSalesManagementTab(){
-        fragmentInvoiceReportBinding.apply {
-            val tabLayout = tabsInvoiceReport
-
-            // 탭 선택 리스너 설정
-            tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
-                override fun onTabSelected(tab: TabLayout.Tab?) {
-                    when(tab?.position){
-                        1-> { // 캘린더
-                            mainActivity.replaceFragment(MainFragmentName.SALES_MANAGEMENT_CALENDAR, true, false, null)
-                        }
-                        2 -> { // 정산 내역
-                            mainActivity.replaceFragment(MainFragmentName.SALES_MANAGEMENT, true, false, null)
-                        }
-                    }
-                }
-
-                override fun onTabUnselected(tab: TabLayout.Tab?) {
-                    //"Not yet implemented"
-                }
-
-                override fun onTabReselected(tab: TabLayout.Tab?) {
-                    //"Not yet implemented"
-                }
-            })
-        }
-    }
 
     // 이전 화면으로 뒤로 가기
     fun backProcess(){
