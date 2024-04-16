971773605f93f87dae30970e9f84ea5f5a529ff0
KIMWONBIN(김원빈) <diffngood@gmail.com>
2024-04-16 15:39:34 +0900

    개별 상품 Model 추가 및 UserModel 추가사항 주석 추가

---
+++ app/src/main/java/kr/co/lion/team4/mrco/model/IndividualProductsModel.kt
@@ -0,0 +1,15 @@
+package kr.co.lion.team4.mrco.model
+
+/*
+위에서 부터 순서대로
+
+인덱스, 상품 카테고리(상의/하의 등), 상품명
+재고 수량, 사이즈, 이미지 파일명, 색상
+*/
+
+data class IndividualProductsModel( var item_idx: Int, var item_category: String, var name: String,
+                                   var stock: Int, var size: Int, var item_image_filename: String, var item_color: Int
+) {
+    // 매개 변수가 없는 생성자 (FireStore 연동 후 사용)
+    constructor(): this(0, "", "", 0, 0, "", 0)
+}
--- app/src/main/java/kr/co/lion/team4/mrco/model/UserModel.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/model/UserModel.kt
@@ -1,6 +1,8 @@
 package kr.co.lion.team4.mrco.model
 
 // 회원번호, 아이디, 비밀번호, 이름, 성별, 이메일, MBTI, 개인정보 제공동의, 상품정보 수신동의, 회원상태
+
+// 닉네임, 전화번호, 주소, 상세주소, 가입일, 키, 체중, 앱 알림 설정, 코디네이터 등록 여부 필요함
 data class UserModel(var userIdx:Int, var userId:String,
                      var userPw:String, var userName:String, var userGender:Int,
                      var userEmail:String, var userMBTI:String, var userConsent01: Boolean,
