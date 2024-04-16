41a3e4704131a64444a0da564573592b08ecaf01
KIMWONBIN(김원빈) <diffngood@gmail.com>
2024-04-16 09:11:41 +0900

    UI Edit

--- app/src/main/java/kr/co/lion/team4/mrco/fragment/mbtiproductmain/MbtiProductBottomFragment.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/fragment/mbtiproductmain/MbtiProductBottomFragment.kt
@@ -37,28 +37,28 @@ class MbtiProductBottomFragment : BottomSheetDialogFragment() {
             val bottomSheetDialog = it as BottomSheetDialog
             // 높이를 설정한다.
             setBottomSheetHeight(bottomSheetDialog)
-
-            // 바텀시트의 배경색을 변경한다 (둥근 모서리를 유지하기 위해 drawable 지정)
-            bottomSheetDialog.window?.setBackgroundDrawableResource(R.drawable.bottom_sheet_background)
         }
 
         return dialog
     }
 
     // BottomSheet의 높이를 설정해준다.
-    fun setBottomSheetHeight(bottomSheetDialog: BottomSheetDialog){
+    fun setBottomSheetHeight(bottomSheetDialog: BottomSheetDialog) {
+
         // BottomSheet의 기본 뷰 객체를 가져온다
         val bottomSheet = bottomSheetDialog.findViewById<View>(com.google.android.material.R.id.design_bottom_sheet)!!
+
+        // 바텀시트의 배경색을 변경한다 (둥근 모서리를 유지하기 위해 drawable 지정)
+        bottomSheet.setBackgroundResource(R.drawable.bottom_sheet_background)
+
         // BottomSheet 높이를 설정할 수 있는 객체를 생성한다.
         val behavior = BottomSheetBehavior.from(bottomSheet)
+
         // 높이를 설정한다.
         val layoutParams = bottomSheet.layoutParams
         layoutParams.height = getBottomSheetDialogHeight()
         bottomSheet.layoutParams = layoutParams
         behavior.state = BottomSheetBehavior.STATE_EXPANDED
-
-        // 바텀시트의 배경색을 변경한다 (둥근 모서리를 유지하기 위해 drawable 지정)
-        // bottomSheet.setBackgroundResource(R.drawable.bottom_sheet_background)
     }
 
     // BottomSheet의 높이를 구한다 (화면 액정의 51% 크기)
