fc8c711708de610748073dedc49a1e59e22f0c6b
KIMWONBIN(김원빈) <diffngood@gmail.com>
2024-04-16 14:58:08 +0900

    코디 상품 등록 페이지 - 추가 등록 부분 - 수정

--- app/src/main/java/kr/co/lion/team4/mrco/viewmodel/productManagement/AddProductDetailViewModel.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/viewmodel/productManagement/AddProductDetailViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.ViewModel
 class AddProductDetailViewModel : ViewModel() {
     // 상품 옵션 명
     val textviewAddProductDetailName = MutableLiveData<String>()
+
     // 상품 옵션 사이즈, 재고 등
     val textviewAddProductDetailOption = MutableLiveData<String>()
 }
