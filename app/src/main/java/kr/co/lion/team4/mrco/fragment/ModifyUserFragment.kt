79aadb4ff916ec45b615dcd0a89440b4cdc8ce26
Boram <dev.lboram@gmail.com>
2024-04-17 18:23:45 +0900

    [Chore] mbti bottomSheet 실행하는 기능 수정

--- app/src/main/java/kr/co/lion/team4/mrco/fragment/ModifyUserFragment.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/fragment/ModifyUserFragment.kt
@@ -5,27 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import kr.co.lion.team4.mrco.databinding.FragmentModifyUserBinding
 import kr.co.lion.team4.mrco.MainActivity
 import kr.co.lion.team4.mrco.MainFragmentName
 import kr.co.lion.team4.mrco.R
+import kr.co.lion.team4.mrco.viewmodel.ModifyUserViewModel
 
 
 class ModifyUserFragment : Fragment() {
 
     lateinit var fragmentModifyUserBinding: FragmentModifyUserBinding
+    lateinit var modifyUserViewModel : ModifyUserViewModel
     lateinit var mainActivity: MainActivity
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
 
-        fragmentModifyUserBinding = FragmentModifyUserBinding.inflate(inflater)
+        fragmentModifyUserBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_modify_user, container, false)
+        modifyUserViewModel = ModifyUserViewModel()
+        fragmentModifyUserBinding.modifyUserViewModel = modifyUserViewModel
         mainActivity = activity as MainActivity
 
         settingToolbar()
 
         // 테스트용 바텀시트 노출 버튼
-        fragmentModifyUserBinding.buttonModifyUserSubmit.setOnClickListener {
+        fragmentModifyUserBinding.textFieldModifyUserUserMBTI.setOnClickListener {
             showMbtiBottomSheet()
         }
 
@@ -47,8 +52,9 @@ class ModifyUserFragment : Fragment() {
 
     // Mbti 바텀 시트
     fun showMbtiBottomSheet(){
-        val mbtiBottomSheetFragment = MbtiBottomSheetFragment()
+        val mbtiBottomSheetFragment = MbtiBottomSheetFragment(modifyUserViewModel.textFieldModifyUserUserMBTI)
         mbtiBottomSheetFragment.show(mainActivity.supportFragmentManager, "MbtiBottomSheet")
+        // to do 선택한 mbti가 edittext에 표기되지 않는 문제 해결
     }
 
     // 뒤로가기 처리
