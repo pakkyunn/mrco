a17e8f25cb5ee51b5b297819f818921e2211f176
문태진 <mtj1015@naver.com>
2024-04-22 15:58:44 +0900

    [Fix] 매출관리화면 연결 임시본1

--- app/src/main/java/kr/co/lion/team4/mrco/viewmodel/salesManagement/InvoiceReportViewModel.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/viewmodel/salesManagement/InvoiceReportViewModel.kt
@@ -2,10 +2,32 @@ package kr.co.lion.team4.mrco.viewmodel.salesManagement
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartModel
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartType
+import com.github.aachartmodel.aainfographics.aachartcreator.AAChartView
+import com.github.aachartmodel.aainfographics.aachartcreator.AASeriesElement
 
 class InvoiceReportViewModel: ViewModel() {
     var invoiceDate = MutableLiveData<String>()
 
     var invoiceReportValue1 = MutableLiveData<String>()
-    
+
+    // 차트 생성
+    val pieChartModel: AAChartModel = AAChartModel()
+        .chartType(AAChartType.Pie)
+        .title("${invoiceDate} 에 결제한 고객 정보 분석")
+        .backgroundColor("ffffff")
+        .dataLabelsEnabled(true)
+        .series(
+            arrayOf(
+                AASeriesElement()
+                    .name("파이 차트 데이터")
+                    .data(arrayOf(
+                        arrayOf("항목1", 10),
+                        arrayOf("항목2", 20),
+                        arrayOf("항목3", 30)
+                    ))
+            )
+        )
+
 }
