214f8b3989dfd518a947684ac19a63050042c365
KIMWONBIN(김원빈) <diffngood@gmail.com>
2024-04-24 14:49:48 +0900

    Update LikeModel.kt

--- app/src/main/java/kr/co/lion/team4/mrco/model/LikeModel.kt
+++ app/src/main/java/kr/co/lion/team4/mrco/model/LikeModel.kt
@@ -6,8 +6,8 @@ package kr.co.lion.team4.mrco.model
 좋아요 인덱스, 회원 인덱스, 상품 인덱스, 좋아요 여부(O, X)
 */
 
-data class LikeModel(var like_idx: Int, var like_user_idx: Int, var like_product_idx: Int, var like_state: Boolean) {
+data class LikeModel(var like_idx: Int, var like_user_idx: Int, var like_product_idx: Int, var product_like_state: Boolean, var coordinator_follow_state: Boolean) {
 
     // 매개 변수가 없는 생성자 (FireStore 연동 후 사용)
-    constructor(): this(0, 0, 0, false)
+    constructor(): this(0, 0, 0, false, false)
 }
